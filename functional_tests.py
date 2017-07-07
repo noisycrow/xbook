aa0973633ded08210ca5a975efd54e1fbf5d827b
Tom Utterback <timba@noisycrow.com>
2017-07-07 14:02:56 -0700

    First FT spec'd out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about...
+        self.browser.get('http://localhost:8000')
+        # she notices the page title...
+        self.assertIn('To-Do', self.browser.title)
+
+        self.fail('Finish the test!')
+
+# she is invited to enter...
+
+# She types "Buy peacock feathers..."
+
+# When she hits enter...
+
+# There is still a text box... she enters "Use peacock feathers..."
+
+# The page updates again...
+
+# Edith wonders... unique URL ...
+
+# She visits that URL ...
+
+# Satisfied, she...
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
