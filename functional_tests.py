aa5f2192486e15ab92f06800b6df38fb077b2158
Tom Utterback <timba@noisycrow.com>
2017-07-07 15:41:40 -0700

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,17 +16,20 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # she notices the page title...
         self.assertIn('To-Do', self.browser.title)
-
-        self.fail('Finish the test!')
-
-# she is invited to enter...
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # she is invited to enter...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 # She types "Buy peacock feathers..."
-
+        inputbox.send_keys('Buy peacock feathers')
 # When she hits enter...
-
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1 : Buy peacock feathers' for row in rows))
 # There is still a text box... she enters "Use peacock feathers..."
-
+        self.fail('Finish the test!')
 # The page updates again...
 
 # Edith wonders... unique URL ...
